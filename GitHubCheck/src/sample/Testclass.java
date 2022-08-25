360172c81613ce41f0dd171d3ff6acbf8efc6702
ART <ART@DESKTOP-CF2VUQT>
2022-08-25 20:37:18 +0530

    commit added print

--- GitHubCheck/src/sample/Testclass.java
+++ GitHubCheck/src/sample/Testclass.java
@@ -15,6 +15,7 @@ public class Testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ragesh statement");
+System.out.println("commit from different system");
 	}
 
 }
