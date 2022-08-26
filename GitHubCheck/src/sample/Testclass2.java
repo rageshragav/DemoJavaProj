83f6e5e38d1bfccdb64a98ef846804593c90f225
ragesh ragav <rageshbecs@gmail.com>
2022-08-26 16:41:50 +0530

    check

--- GitHubCheck/src/sample/Testclass2.java
+++ GitHubCheck/src/sample/Testclass2.java
@@ -15,6 +15,8 @@ public class Testclass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ragesh statement");
+
+System.out.println("Ragesh statement");
 System.out.println("commit from different system");
 //---------------------------------------------------
 System.out.println("kashyap statement");
