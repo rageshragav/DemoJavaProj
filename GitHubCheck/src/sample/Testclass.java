3acf1bd9f6b68b1c338827a46cc7806da162ce0e
ragesh ragav <rageshbecs@gmail.com>
2022-08-26 13:35:21 +0530

    Merge conflict

--- GitHubCheck/src/sample/Testclass.java
+++ GitHubCheck/src/sample/Testclass.java
@@ -14,8 +14,7 @@ public class Testclass {
 	/**
 	 * @param args
 	 */
-<<<<<<< HEAD
-=======
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Ragesh statement");
@@ -25,7 +24,7 @@ System.out.println("kashyap statement");
 System.out.println("commit from different system");
 	}
 
->>>>>>> branch 'master' of https://github.com/rageshragav/DemoJavaProj.git
+
 }
 	
 
