dc8ded5f32017bbbe1e41b12c441dbd9b368265e
ragesh ragav <rageshbecs@gmail.com>
2022-08-26 13:32:24 +0530

    Converted the project to Maven and added the required dependencies

--- GitHubCheck/src/sample/Testclass.java
+++ GitHubCheck/src/sample/Testclass.java
@@ -3,6 +3,8 @@
  */
 package sample;
 
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * @author ragesh
  *
@@ -12,10 +14,7 @@ public class Testclass {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Ragesh statement");
-System.out.println("commit from different system");
-	}
-
 }
+	
+
+
