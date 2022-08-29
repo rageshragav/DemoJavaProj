863d1531b97045ea17c4a699fbe6395b7b0283fa
ragesh ragav <rageshbecs@gmail.com>
2022-08-29 10:24:29 +0530

    Added source folders and required packages

---
+++ GitHubCheck/src/testcases/Testclass.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+
+/**
+ * @author ragesh
+ *
+ */
+public class Testclass {
+
+	/**
+	 * @param args
+	 */
+public	BaseClass loginpg;
+	
+	@Test
+	public void login() {
+		
+		
+	}
+
+
+}
+	
+
+
